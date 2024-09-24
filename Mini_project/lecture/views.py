1dca3abf1edc088fc0f7924e339d5b057dd94ff1
SKKU-07\user <smalbear@skkukdp.re.kr>
2024-09-24 18:20:22 +0900

    page number 해보기 - 미완료

--- Mini_project/lecture/views.py
+++ Mini_project/lecture/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from lecture.models import Categories, Lectures, Comments
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -12,12 +13,16 @@ def lecture_list(request):
     category_list = Categories.objects.all()
     lecture_list = Lectures.objects.prefetch_related(
         "comments_set").all()[start:end]
+
+    page_numbers = list(range(start + 1, end + 2))
+
     response = render(
         request,
         "lecture/lecture-list.html",
         {
             "category_list": category_list,
-            "lecture_list": lecture_list
+            "lecture_list": lecture_list,
+            "page_numbers": page_numbers,
         },
     )
     return response
