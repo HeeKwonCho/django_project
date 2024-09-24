c08dbccfda7a7410f81fe31a8ba4428694e72072
KKT-DESKTOP\kwang <smalbear@naver.com>
2024-09-24 22:08:57 +0900

    page 번호 처리, but 중앙 처리가 필요할 것임

--- Mini_project/lecture/views.py
+++ Mini_project/lecture/views.py
@@ -5,24 +5,31 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def lecture_list(request):
-    page = int(request.GET.get("page", 1))
-    PAGE_SIZE = 20
+    # page = int(request.GET.get("page", 1))
+    # PAGE_SIZE = 20
 
-    start, end = (page - 1) * PAGE_SIZE, page * PAGE_SIZE
+    # start, end = (page - 1) * PAGE_SIZE, page * PAGE_SIZE
 
+    # category_list = Categories.objects.all()
+    # lecture_list = Lectures.objects.prefetch_related(
+    #     "comments_set").all()[start:end]
+
+    # page_numbers = list(range(start + 1, end + 2))
     category_list = Categories.objects.all()
-    lecture_list = Lectures.objects.prefetch_related(
-        "comments_set").all()[start:end]
+    lecture_list = Lectures.objects.prefetch_related("comments_set").all()
+    
+    PAGE_SIZE = 20
+    paginator = Paginator(lecture_list, PAGE_SIZE)
 
-    page_numbers = list(range(start + 1, end + 2))
+    page_number = request.GET.get("page")
+    lecture_page_obj = paginator.get_page(page_number)
 
     response = render(
         request,
         "lecture/lecture-list.html",
         {
             "category_list": category_list,
-            "lecture_list": lecture_list,
-            "page_numbers": page_numbers,
+            "lecture_list": lecture_page_obj,
         },
     )
     return response
