7af667a9302d8a21340ab067c6f5b2d43e2c2a51
KKT-DESKTOP\kwang <smalbear@naver.com>
2024-09-24 21:50:11 +0900

    추가한 컬럼 반영 및 오류 수정

--- Mini_project/data_temp_conversion.sql
+++ Mini_project/data_temp_conversion.sql
@@ -24,39 +24,41 @@ CREATE TABLE `lecture_temp` (
 -- 7. 아래 순차적으로 수행
 
 # 1. lecture_sites
-insert into lecture_sites (site_name, site_url)
-values ('Udemy', 'https://www.udemy.com/')
+insert into lecture_sites (site_name, site_url, created_at, updated_at)
+values ('Udemy', 'https://www.udemy.com/', now(), now())
 ;
-insert into lecture_sites (site_name, site_url)
-values ('Inflearn', 'https://www.inflearn.com/')
+insert into lecture_sites (site_name, site_url, created_at, updated_at)
+values ('Inflearn', 'https://www.inflearn.com/', now(), now())
 ;
 
 # 2. lecture_lecturer
-insert into lecture_lecturer (lecturer_name)
-select distinct lt.institution
+insert into lecture_lecturers (lecturer_name, created_at, updated_at)
+select distinct lt.institution, now(), now()
 from   lecture_temp lt
 ;
 
 # 3. lecture_categories
 # 3-1. Udemy level 1
-insert into lecture_categories (cat_name)
+insert into lecture_categories (cat_name, created_at, updated_at)
 select distinct case when instr(lt.field, '/') = 0 then
                           lt.field
                      else substr(lt.field, 1, instr(lt.field, '/') - 1)
                 end as cat
+               ,now(), now() 
 from   lecture_temp lt
 where  instr(lt.detail_link, 'udemy') > 0
 ;
 # 3-2. Inflearn level 1
-insert into lecture_categories (cat_name)
+insert into lecture_categories (cat_name, created_at, updated_at)
 select distinct substr(substr(lt.field, 1, instr(lt.field, '>') - 1), 1, instr(lt.field, '/') - 1) as cat
+               ,now(), now() 
 from   lecture_temp lt
 where  instr(lt.detail_link, 'inflearn') > 0
 ;
 # level 2 는 포기
 
 # 4. lecture_lectures
-insert into lecture_lectures (site_id, category_id, lecturer_id, lec_title, lec_img_link, lec_link, rating, review_count)
+insert into lecture_lectures (site_id, category_id, lecturer_id, lec_title, lec_img_link, lec_link, rating, review_count, created_at, updated_at)
 select (select ls.id
         from   lecture_sites ls
         where  instr(lt.detail_link, ls.site_url) > 0) as site_id
@@ -78,6 +80,8 @@ select (select ls.id
       ,lt.detail_link
       ,lt.satisfaction
       ,lt.review_count
+      ,now()
+      ,now() 
 from   lecture_temp lt
 ;
 
