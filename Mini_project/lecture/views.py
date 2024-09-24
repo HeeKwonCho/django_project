773cc85b214cc90f2676fa9579d9a4e25b27bc96
SKKU-07\user <smalbear@skkukdp.re.kr>
2024-09-24 17:31:05 +0900

    숫자 , 처리와 페이징 기능 추가

--- Mini_project/lecture/views.py
+++ Mini_project/lecture/views.py
@@ -4,8 +4,14 @@ from lecture.models import Categories, Lectures, Comments
 
 # Create your views here.
 def lecture_list(request):
+    page = int(request.GET.get("page", 1))
+    PAGE_SIZE = 20
+
+    start, end = (page - 1) * PAGE_SIZE, page * PAGE_SIZE
+
     category_list = Categories.objects.all()
-    lecture_list = Lectures.objects.prefetch_related("comments_set").all()
+    lecture_list = Lectures.objects.prefetch_related(
+        "comments_set").all()[start:end]
     response = render(
         request,
         "lecture/lecture-list.html",
