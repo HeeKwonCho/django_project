26f62e5ca53c480eb44aca0aa90182d4a97b533b
SKKU-02\Skku <gmlrnjswh@skkukdp.re.kr>
2024-09-25 13:32:01 +0900

    페이지네이션 작성

--- Mini_project/Mini_project/urls.py
+++ Mini_project/Mini_project/urls.py
@@ -16,13 +16,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include('courses.urls')),
-]
-=======
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -30,4 +23,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("lecture/", include("lecture.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> origin/main
